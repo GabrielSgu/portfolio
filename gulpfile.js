7c2f3422854c953dfa15f4c3b673110992850247
GabrielSgu <gabrielsg@id.uff.br>
2017-03-08 00:00:49 -0300

    gulp fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
 
+var sass = require('gulp-sass');
+
 gulp.task('default',function() {
 
 
+});
+
+gulp.task('sass',function() {
+
+    return gulp.src('./css/main.scss').pipe(sass()).pipe(gulp.dest('./css'))
+    
+    
+});
+
+
+gulp.task('watch', function(){
+  gulp.watch('./css/main.scss', ['sass']); 
+  
 });
--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+(function(){
+  
+    var thumbnails = {
 
+	thumbnails_array:  [[1,"blue"],[2,"red"],[3,"yellow"],
+                      [4,"brown"],[5,"green"],[6,"purple"]],
+	init:function(){
+	    this.cacheDom();
+	    this.render();
+	},
+
+	cacheDom:function(){
+	    this.$el = $("#portifolio-thumbnail-group");
+	},
+
+	render:function(){
+	    console.log("entered render!");
+	    console.log(this.$el);
+	    for(var i =  0; i < this.thumbnails_array.length; i++){
+		var $thumb = $("<div id=" + this.thumbnails_array[i][0] + 
+			       " class='portfolio-thumbnail'></div>");
+		console.log("<div id="+ this.thumbnails_array[i][0] + 
+			    " class='portfolio-thumbnail'></div>");
+		$thumb.css("background-color",this.thumbnails_array[i][1]);
+		$("#portfolio-thumbnail-group").append($thumb);
+		console.log("looped: " + i);
+	    }
+	}
+    };
+
+    thumbnails.init();
+    
+})()
